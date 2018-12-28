d5706c9e2d80b7d748ef814c76f9b42d289bedc8
Hemanth SV <hsv@Hemanths-MacBook-Pro.local>
2018-12-28 17:02:05 +0530

    changed app text

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Hi there. Edit file <code>src/App.js</code> and save to reload.
+            Hi there. This is feature 1. Edit file <code>src/App.js</code> and save to reload.
           </p>
           <a
             className="App-link"
